e772bb287e6d40b9ea7e417a70a73266615a7ebf
Jeremy Allinder <31550962+jballinder@users.noreply.github.com>
2019-07-09 08:52:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(() => {
     $('.menu-toggler').on('click',
         function() {
             $(this).toggleClass('open');
@@ -6,13 +6,13 @@ $(document).ready(function() {
         });
 
     $('.top-nav .nav-link').on('click',
-        function() {
+        () => {
             $('.menu-toggler').removeClass('open');
             $('.top-nav').removeClass('open');
         });
 
     $('nav a[href*="#"]').on('click',
-        function() {
+        () => {
             $('html, body').animate(keyframes, {
                 scrollTop: $($(this).attr('href')).offset().top - 100
             }, options, 2000);
